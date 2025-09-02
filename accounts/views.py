f87324984be6cc058a6f34591752762e2e996b7b
yasuprog <yasuprog@gmail.com>
2025-09-02 18:14:40 +0900

    ログイン関連のプログラム作成

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+def signup(reqest):
+    if reqest.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        else:
+            form = UserCreationForm()
+        return render(reqest, 'accounts/signup.html', {'form': form})
